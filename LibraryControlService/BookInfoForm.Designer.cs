fcc34bf0bdd675e8271c9fb3f3a1c1d06abc96f9
AmletixQ <nazarovamridin308@gmail.com>
2026-01-17 21:21:18 +0300

    Сделал взаимодействие форм MainForm и BookInfoForm.

--- LibraryControlService/BookInfoForm.Designer.cs
+++ LibraryControlService/BookInfoForm.Designer.cs
@@ -35,6 +35,8 @@
             BuyButton = new Button();
             BasketButton = new Button();
             BookPriceLabel = new Label();
+            label1 = new Label();
+            label2 = new Label();
             ((System.ComponentModel.ISupportInitialize)BookCoverPictureBox).BeginInit();
             SuspendLayout();
             // 
@@ -50,7 +52,8 @@
             // 
             BookNameLabel.AutoSize = true;
             BookNameLabel.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
-            BookNameLabel.Location = new Point(138, 12);
+            BookNameLabel.Location = new Point(134, 12);
+            BookNameLabel.MaximumSize = new Size(270, 0);
             BookNameLabel.Name = "BookNameLabel";
             BookNameLabel.Size = new Size(76, 30);
             BookNameLabel.TabIndex = 1;
@@ -59,26 +62,30 @@
             // BookAuthorLabel
             // 
             BookAuthorLabel.AutoSize = true;
-            BookAuthorLabel.Location = new Point(138, 66);
+            BookAuthorLabel.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            BookAuthorLabel.Location = new Point(138, 110);
             BookAuthorLabel.Name = "BookAuthorLabel";
-            BookAuthorLabel.Size = new Size(38, 15);
+            BookAuthorLabel.Size = new Size(48, 20);
             BookAuthorLabel.TabIndex = 2;
             BookAuthorLabel.Text = "label1";
             // 
             // BookDescriptionLabel
             // 
             BookDescriptionLabel.AutoSize = true;
-            BookDescriptionLabel.Location = new Point(12, 260);
+            BookDescriptionLabel.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            BookDescriptionLabel.Location = new Point(12, 232);
+            BookDescriptionLabel.MaximumSize = new Size(390, 0);
             BookDescriptionLabel.Name = "BookDescriptionLabel";
-            BookDescriptionLabel.Size = new Size(38, 15);
+            BookDescriptionLabel.Size = new Size(50, 20);
             BookDescriptionLabel.TabIndex = 3;
             BookDescriptionLabel.Text = "label1";
             // 
             // BuyButton
             // 
-            BuyButton.Location = new Point(55, 358);
+            BuyButton.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            BuyButton.Location = new Point(12, 331);
             BuyButton.Name = "BuyButton";
-            BuyButton.Size = new Size(138, 38);
+            BuyButton.Size = new Size(187, 38);
             BuyButton.TabIndex = 4;
             BuyButton.Text = "Купить";
             BuyButton.UseVisualStyleBackColor = true;
@@ -86,9 +93,10 @@
             // 
             // BasketButton
             // 
-            BasketButton.Location = new Point(199, 358);
+            BasketButton.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            BasketButton.Location = new Point(205, 331);
             BasketButton.Name = "BasketButton";
-            BasketButton.Size = new Size(138, 38);
+            BasketButton.Size = new Size(188, 38);
             BasketButton.TabIndex = 5;
             BasketButton.Text = "В корзину";
             BasketButton.UseVisualStyleBackColor = true;
@@ -97,17 +105,38 @@
             // BookPriceLabel
             // 
             BookPriceLabel.AutoSize = true;
-            BookPriceLabel.Location = new Point(12, 154);
+            BookPriceLabel.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            BookPriceLabel.Location = new Point(10, 166);
             BookPriceLabel.Name = "BookPriceLabel";
-            BookPriceLabel.Size = new Size(38, 15);
+            BookPriceLabel.Size = new Size(65, 25);
             BookPriceLabel.TabIndex = 6;
             BookPriceLabel.Text = "label1";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 151);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Цена:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 208);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Описание:";
             // 
             // BookInfoForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(388, 408);
+            ClientSize = new Size(405, 379);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(BookPriceLabel);
             Controls.Add(BasketButton);
             Controls.Add(BuyButton);
@@ -131,5 +160,7 @@
         private Button BuyButton;
         private Button BasketButton;
         private Label BookPriceLabel;
+        private Label label1;
+        private Label label2;
     }
 }
